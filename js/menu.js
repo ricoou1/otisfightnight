5393040ce30177507230a30d2d35c69bac2afa2b
ricoou1 <richard.androsov@gmail.com>
2025-06-05 21:11:25 +0200

    UploadUpadate

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,11 @@
 const burgerIcon = document.querySelector('.fa-bars');
-        const menu = document.getElementById('menuLinks');
+const menu = document.getElementById('menuLinks');
 
-        burgerIcon.addEventListener('click', (e) => {
-            e.stopPropagation();
-            menu.classList.toggle('active');
-        });
+burgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menu.classList.toggle('active');
+});
 
-        menu.addEventListener('click', (e) => {
-            e.stopPropagation();
-        });
-
-        document.addEventListener('click', () => {
-            menu.classList.remove('active');
-        });
+document.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
